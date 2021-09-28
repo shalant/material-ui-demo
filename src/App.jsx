77221592ea9d85c8a1f4602220f7773627ad91d3
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-09-27 22:27:17 -0500

    3334, very interesting

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Typography, AppBar, Button, Item, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
+import useStyles from './styles';
+
+const App = () => {
+    const classes = useStyles();
+    return (
+        <>
+            <CssBaseline />
+            <AppBar position='relative'>
+                <Toolbar>
+                    <PhotoCamera />
+                    <Typography variant='h6'>
+                        Photo Album
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div className={classes.container} >
+                    <Container maxWidth='sm'>
+                        <Typography variant='h2' align='center' color='textPrimary' gutterBottom>
+                            Photo Album
+                        </Typography>
+                        <Typography variant='h5' align='center' color='textSecondary' paragraph>
+                            Hello everyone, this is a photo album and I'm trying to make this sentence as long as possible
+                        </Typography>
+                        <div>
+                            <Grid container spacing={2} justify='center'>
+                                <Grid item>
+                                    <Button variant='contained' color='primary'>
+                                        See My Photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant='outlined' color='primary'>
+                                        Secondary Action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
+                    </Container>
+                </div>
+            </main>
+        </>
+    )
+}
+
+export default App;
