cffc205a1a8c6c40cd038c55aeec405c522c23b1
Douglas Rosenberg <doug.rosenberg@gmail.com>
2021-09-29 22:07:52 -0500

    very cool intro to material-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Typography, AppBar, Button, Item, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
+
 import useStyles from './styles';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const App = () => {
     const classes = useStyles();
@@ -10,7 +13,7 @@ const App = () => {
             <CssBaseline />
             <AppBar position='relative'>
                 <Toolbar>
-                    <PhotoCamera />
+                    <PhotoCamera className={classes.icon} />
                     <Typography variant='h6'>
                         Photo Album
                     </Typography>
@@ -25,7 +28,7 @@ const App = () => {
                         <Typography variant='h5' align='center' color='textSecondary' paragraph>
                             Hello everyone, this is a photo album and I'm trying to make this sentence as long as possible
                         </Typography>
-                        <div>
+                        <div className={classes.buttons}>
                             <Grid container spacing={2} justify='center'>
                                 <Grid item>
                                     <Button variant='contained' color='primary'>
@@ -41,7 +44,42 @@ const App = () => {
                         </div>
                     </Container>
                 </div>
+                <Container className={classes.cardGrid} maxWidth='md'>
+                    <Grid container spacing={4}>
+                    {cards.map((card) => (
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                            <Card className={classes.card}>
+                                <CardMedia 
+                                    className={classes.cardMedia}
+                                    image='https://source.unsplash.com/random'
+                                    title='Image title'
+                                />
+                                <CardContent className={classes.cardContent}>
+                                    <Typography gutterBottom variant='h5'>
+                                        Heading
+                                    </Typography>
+                                    <Typography>
+                                        This is a media card, you can use this section to describe the content
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size='small' color='primary'>View</Button>
+                                    <Button size='small' color='primary'>Edit</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                    ))}
+                    </Grid>
+                </Container>
             </main>
+            <footer className={classes.footer}>
+                <Typography variant='h6' align='center' gutterBottom>
+                        Footer
+                </Typography>
+                <Typography variant='subtitle1' align='center' color='textSecondary'>
+                        Something here to give the footer a purpose
+                </Typography>
+            </footer>
         </>
     )
 }
